cbed8b9924c61c9abc854f5aa39c27f33903b0e5
purisantosh <116016459+purisantosh@users.noreply.github.com>
2026-02-23 22:42:11 -0500

    add swagger open api documentation

--- src/main/java/com/example/ems/EmsBackendApplication.java
+++ src/main/java/com/example/ems/EmsBackendApplication.java
@@ -1,9 +1,21 @@
 package com.example.ems;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "Employee Management System",
+		version = "1.0",
+		description = "This is a REST API with end points for EMS"
+),
+servers = {
+		@Server(url ="http://localhost:8080", description = "Local server")
+})
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
